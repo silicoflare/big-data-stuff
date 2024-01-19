473e30804c48b78c3ab6fe25f331e7195f59e7fb
Suraj B M <silicoflare@gmail.com>
2024-01-19 10:45:04 +0530

    Fix package name in docker setup script

--- setup-docker.sh
+++ setup-docker.sh
@@ -2,6 +2,6 @@
 # Docker Setup Script
 # Run this first if you're using/testing hadoop script on an Ubuntu Docker image
 
-apt install -y sudo wget nvim                   # install basic dependencies
+apt install -y sudo wget neovim                   # install basic dependencies
 adduser user
 usermod -aG sudo user
